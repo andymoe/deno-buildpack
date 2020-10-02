2dfde0ede3ab03937e36e74faf0a5162eb37aa3d
Andy Brown <andymoe@gmail.com>
2020-10-02 01:25:14 -0700

    Implement real deno detection
    - Introduce DetectDeno which shorts circuts and succeeds if it finds a
      deps.ts file. If it can't find deps.ts then it walks source code with
      .ts or .js extensions looking for references to the deno std lib or
      calls to the top level Deno object.
    
    - It's probably past time to write some tests.

--- deno/detect.go
+++ deno/detect.go
@@ -1,21 +1,90 @@
 package deno
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/paketo-buildpacks/packit"
 )
 
 // Detect figures out if we are working with a deno app
 func Detect() packit.DetectFunc {
 	return func(context packit.DetectContext) (packit.DetectResult, error) {
+		ok := DetectDeno(context.WorkingDir)
+		if ok {
+			return packit.DetectResult{
+				Plan: packit.BuildPlan{
+					Provides: []packit.BuildPlanProvision{
+						{Name: "deno"},
+					},
+					Requires: []packit.BuildPlanRequirement{
+						{Name: "deno"},
+					},
+				},
+			}, nil
+		}
+
 		return packit.DetectResult{
 			Plan: packit.BuildPlan{
 				Provides: []packit.BuildPlanProvision{
 					{Name: "deno"},
 				},
-				Requires: []packit.BuildPlanRequirement{
-					{Name: "deno"},
-				},
+				Requires: []packit.BuildPlanRequirement{},
 			},
 		}, nil
 	}
+}
+
+// DetectDeno scans *.ts or *.js source looking for signs of deno
+// It's probably a little too greedy right now
+func DetectDeno(workingDir string) (ok bool) {
+	if fileExists(filepath.Join(workingDir, "deps.ts")) {
+		return true
+	}
+
+	denoFound := false
+	err := filepath.Walk(workingDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		ext := filepath.Ext(path)
+		if !(".ts" == ext || ".js" == ext) {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			if bytes.Contains(scanner.Bytes(), []byte("://deno.land/std")) {
+				denoFound = true
+				return fmt.Errorf("Deno source found in %v", path)
+			}
+
+			if bytes.Contains(scanner.Bytes(), []byte("Deno.")) {
+				denoFound = true
+				return fmt.Errorf("Deno source found in %v", path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("There was an error scanning the source code: %v", err)
+	}
+
+	return denoFound
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
 }
